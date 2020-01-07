a66405c49d60b0b10067ee015586366c31663b47
Stijnantoine99 <Stijnantoine99>
2020-01-07 14:45:19 +0100

    placing all houses random. with map

--- code/algoritmes/day2_placing.py
+++ code/algoritmes/day2_placing.py
@@ -1,39 +1,12 @@
-# Random huizen plaatsen
-
-# 20 huizen
-# 12 eensgezinswoningen
-# 5 bungalows
-# 3 maisons
-
 import random
 import csv
-
-# xlist = []
-# for i in range(0,160):
-#     xlist.append("i")
-# print(xlist)
-
-# ylist = []
-# for i in range(0,180):
-#     ylist.append("i")
-# # print(ylist)
-
-# x = random.choice(xlist[2:158])
-# y = random.choice(ylist[2:178])
-
-# x = random.randrange(2,158)
-# y = random.randrange(2,158)
-
-# print(x,y)
-
-# xlist[x] = "X"
-# print(xlist)
-
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.colors
 
 x, y = (160, 180) 
 wijk1 = [[0 for i in range(x)] for j in range(y)] 
 counter = 0 
-# print(wijk1)
 
 while counter < 12: 
     x = random.randrange(32,158)
@@ -43,29 +16,17 @@ while counter < 12:
         x = random.randrange(32,158)
         y_oud = random.randrange(2,178)
 
-    print(x,y_oud)
-    # wijk1[x][y_oud] = 'H'
-    # print(wijk1)
-
-    # for i in range(0,8):
-    #     x += 1
-    #     print(x)
-    #     wijk1[x][y_oud] = "H"
-    #     for j in range(0,8):
-    #         y = y_oud + j
-    #         print(y)
-    #         wijk1[x][y] = "H"
     if wijk1[y_oud][x+8] or wijk1[y_oud+8][x] != 0:
         print("Break")
     else: 
         for i in range(0,8):
             x += 1
             print(x)
-            wijk1[y_oud][x] = "E"
+            wijk1[y_oud][x] = 1
             for j in range(0,8):
                 y = y_oud + j
                 print(y)
-                wijk1[y][x] = "E"  
+                wijk1[y][x] = 1  
         counter += 1  
 
 
@@ -84,11 +45,11 @@ while counter_maison < 3:
         for i in range(0,12):
             x += 1
             print(x)
-            wijk1[y_oud][x] = "M"
+            wijk1[y_oud][x] = 3
             for j in range(0,10):
                 y = y_oud + j
                 print(y)
-                wijk1[y][x] = "M"  
+                wijk1[y][x] = 3 
         counter_maison += 1  
 
 counter_bungalow = 0
@@ -106,23 +67,20 @@ while counter_bungalow < 5:
         for i in range(0,11):
             x += 1
             print(x)
-            wijk1[y_oud][x] = "B"
+            wijk1[y_oud][x] = 2
             for j in range(0,7):
                 y = y_oud + j
                 print(y)
-                wijk1[y][x] = "B"  
+                wijk1[y][x] = 2
         counter_bungalow += 1  
 
 
+H = np.array(wijk1)
+plt.imshow(H)
+plt.pcolor(H, cmap = "Greens_r")
 
-
-
-
-
-
-
-
-# print(wijk1)
+plt.show()
+plt.savefig("blueprint.png")
 
 with open("wijk1.csv","w+") as my_csv:
     csvWriter = csv.writer(my_csv,delimiter=',')
